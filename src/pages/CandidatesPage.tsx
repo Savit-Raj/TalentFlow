529a845273b0ac6f783398c201acb0bbd3ee259d
Savit-Raj <btech10303.22@bitmesra.ac.in>
2025-09-28 16:59:43 +0530

    Adding New features in Candidates page

---
+++ src/pages/CandidatesPage.tsx
@@ -0,0 +1,12 @@
+/**
+ * Candidates Page Component
+ * Main page for the Candidates functionality
+ */
+
+import CandidatesBoard from '@/components/candidates/CandidatesBoard';
+
+const CandidatesPage = () => {
+  return <CandidatesBoard />;
+};
+
+export default CandidatesPage;
